de3cac8c037f1daabae87d907e20607766d25fd7
mgm2023-trees <138403811+mgm2023-trees@users.noreply.github.com>
2023-07-03 13:36:53 +0100

    lineage_beyondx: adapt to derpfest

--- derp_beyondx.mk
+++ derp_beyondx.mk
@@ -24,6 +24,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 ## Inscreen Fingerprint HAL
 TARGET_HAVE_FOD := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
+
+## Faceunlock 
+TARGET_FACE_UNLOCK_SUPPORTED:= true
 
 ## Inherit from beyondx device
 $(call inherit-product, device/samsung/beyondx/device.mk)
@@ -32,15 +37,20 @@ $(call inherit-product, device/samsung/beyondx/device.mk)
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
+TARGET_BOOT_ANIMATION_RES := 1440
 
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+## Inherit some common Derpfest stuff
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+#Official Tag
+DERP_BUILDTYPE := Official
+DERP_MAINTAINER := Mgm2023
 
 ## Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := lineage_beyondx
+PRODUCT_NAME := derp_beyondx
 PRODUCT_DEVICE := beyondx
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G977B
